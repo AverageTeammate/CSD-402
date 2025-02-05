764677dbb85ab2d3d9cbaa91948b23ecb0c43005
unknown <jacob.cannamela784@gmail.com>
2025-02-05 15:34:45 -0500

    deleting screen shots

--- module-7/ScopeExample.java
+++
@@ -1,9 +0,0 @@
-public class ScopeExample {
-    int instanceVar = 5; // Instance variable
-
-    public void display() {
-        int localVar = 10; // Local variable
-        System.out.println("Instance Variable: " + instanceVar);
-        System.out.println("Local Variable: " + localVar);
-    }
-}
